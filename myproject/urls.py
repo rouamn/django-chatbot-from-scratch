8cbcb16d3b6d4e56438d7d4c487edb50bf954d84
Sonia <soniachalouah1@gmail.com>
2024-04-29 23:55:06 +0100

    text summarizer

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,8 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from .chatbotView import chat
+from .summarizerViews import summarize
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chatbot/', chat, name='chatbot'),
+    path('summarize/', summarize, name='summarize'),
+    
 ]
