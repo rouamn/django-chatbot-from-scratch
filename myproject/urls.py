45d452b699f3f4c36799d6ee1c727a672a1a4c65
Sonia-Chalouah <soniachalouah1@gmail.com>
2024-04-30 15:35:42 +0100

    pdf-summarizer

--- myproject/urls.py
+++ myproject/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.urls import path
 from .chatbotView import chat
 from .summarizerViews import summarize
-
+from .pdfsummarizerViews import summarize_pdf_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chatbot/', chat, name='chatbot'),
     path('summarize/', summarize, name='summarize'),
+    path('summarize-pdf/', summarize_pdf_view, name='summarize_pdf'),
     
 ]
